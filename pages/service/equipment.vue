33697e9eabfa1d1f329c2b6091d432c86c7c3a91
anlondon@foxmail.com <gitlabpassword>
2024-06-03 17:30:06 +0800

    fix: 去除一些不必要的console.log

--- pages/service/equipment.vue
+++ pages/service/equipment.vue
@@ -320,9 +320,7 @@
 						that.xz_all = [] // 清空选中项
 						//遍历当前设备
 						that.all.forEach(item=>{
-							console.log(item)
 							if(item.qrcode!=null && item.qrcode['id'] == scan_id){
-								console.log(item.qrcode['id'])
 								that.xz_all = [item.id]
 							}
 						})
