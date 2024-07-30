7adcbc11a45160f0d9c2fea9bece3adfbe8ebf43
koupeng <1593135101@qq.com>
2024-07-30 09:12:07 +0800

    ss

--- common/config.js
+++ common/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://uat.teach.lbsapps.cn/api' //api.technician.com   uat.teach.lbsapps.cn   v1.teach.lbsapps.cn
+export const BASE_URL = 'https://api.technician.com/api' //api.technician.com   uat.teach.lbsapps.cn   v1.teach.lbsapps.cn
 // export const BASE_URL = 'http://localhost:10006/api'
