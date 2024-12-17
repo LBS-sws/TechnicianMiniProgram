d8a1b498d1c0aaff732e42e350fceca35111f3ee
koupeng <1593135101@qq.com>
2024-12-17 14:46:01 +0800

    新拉的代码

--- common/config.js
+++ common/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://uat.teach.lbsapps.cn/api' //api.technician.com   uat.teach.lbsapps.cn   v1.teach.lbsapps.cn
+export const BASE_URL = 'https://api.technician.com/api' //api.technician.com   uat.teach.lbsapps.cn   v1.teach.lbsapps.cn
 // export const BASE_URL = 'http://localhost:10006/api'
 //记得改 main.js:64的图片上传地址
