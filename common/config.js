56850478f2ded72da6c7105885975af9024df933
koupeng <1593135101@qq.com>
2025-04-30 22:51:58 +0800

    s

--- common/config.js
+++ common/config.js
@@ -2,4 +2,4 @@
 
 // export const BASE_URL = 'https://v1.teach.lbsapps.cn/api' //api.technician.com   uat.teach.lbsapps.cn   v1.teach.lbsapps.cn
 // export const BASE_URL = 'http://localhost:10005/api' //api.technician.com   uat.teach.lbsapps.cn   v1.teach.lbsapps.cn
-export const BASE_URL = 'http://uat.teach.lbsapps.cn/api' //api.technician.com   uat.teach.lbsapps.cn   v1.teach.lbsapps.cn
+export const BASE_URL = 'https://uat.teach.lbsapps.cn/api' //api.technician.com   uat.teach.lbsapps.cn   v1.teach.lbsapps.cn
