402023b8e9e4a859e1e229e013862f6ff8de9d2e
koupeng <1593135101@qq.com>
2025-06-03 14:21:26 +0800

    ss

--- common/config.js
+++ common/config.js
@@ -1,6 +1,7 @@
 //记得改 main.js:64的图片上传地址
 
 // export const BASE_URL = 'https://v1.teach.lbsapps.cn/api'
-export const BASE_URL = 'https://uat.teach.lbsapps.cn/api'  //api.technician.com   uat.teach.lbsapps.cn  kp.teach.lbsapps.cn  v1.teach.lbsapps.cn
+export const BASE_URL = 'https://api.technician.com/api'  //api.technician.com   uat.teach.lbsapps.cn  kp.teach.lbsapps.cn  v1.teach.lbsapps.cn
 // export const BASE_URL = 'http://localhost:10005/api' 
 
+	
